f0b7223e6036adbb564154b8683c77e70dbe18a8
QuaKim <121525213+QuaKim@users.noreply.github.com>
2026-07-30 15:47:40 +0200

    Add constants for obrelaporta_wcity integration

---
+++ custom_components/obrelaporta_wcity/const.py
@@ -0,0 +1,8 @@
+DOMAIN = "obrelaporta_wcity"
+
+CONF_USERNAME = "username"
+CONF_PASSWORD = "password"
+
+URL_BASE = "https://obrelaporta.wcity.app"
+URL_API_ADRECA = f"{URL_BASE}/modules/WCITY/api/v1/usuario/adreca"
+URL_API_CALENDARI_AVUI = f"{URL_BASE}/modules/WCITY/api/v1/sector/calendari?que=avui"
